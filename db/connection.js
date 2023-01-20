3c123db2b77dc68890e501690f74ac19f2fc3129
Roseanna Craggs <roseannacraggs21@gmail.com>
2023-01-20 16:02:34 +0000

    updated for hosting

--- db/connection.js
+++ db/connection.js
@@ -9,4 +9,17 @@ if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or PGDATABASE_URL not set')
+}
+
+const config =
+ENV === 'production'
+? {
+  connectionString: process.env.DATABASE_URL,
+  max: 2,
+} 
+: {};
+
 module.exports = new Pool();
+module.exports = new Pool (config);
