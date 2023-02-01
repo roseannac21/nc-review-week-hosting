c7a91601ea07f66340ea9024293aeb648f634164
Roseanna Craggs <roseannacraggs21@gmail.com>
2023-02-01 14:28:57 +0000

    updates for hosting

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,10 @@
 const { Pool } = require('pg');
+const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or PGDATABASE_URL not set')
@@ -21,5 +18,4 @@ ENV === 'production'
 } 
 : {};
 
-module.exports = new Pool();
-module.exports = new Pool (config);
+module.exports = new Pool(config);
